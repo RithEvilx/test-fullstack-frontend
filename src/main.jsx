119a79c9b68c87cf511e846b6ef54bd4a0e8469a
RithEvil <sovannarith22317@gmail.com>
2025-04-20 21:42:28 +0700

    lastest v2

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Users from "./User/Users.jsx";
 import CreateUser from "./User/CreateUser.jsx";
 import UpdateUser from "./User/UpdateUser.jsx";
-import UploadImage from "./UploadImage.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -15,7 +13,6 @@ createRoot(document.getElementById("root")).render(
         <Route path="/" element={<Users />} />
         <Route path="/createUser" element={<CreateUser />} />
         <Route path="/updateUser/:id" element={<UpdateUser />} />
-        <Route path="/uploadimage" element={<UploadImage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
